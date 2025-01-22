88bc3c47372c5d619c3da3d2b69e460ee3177f5f
Hubert-Gasior <164912381+Hubert-Gasior@users.noreply.github.com>
2025-01-22 17:37:50 +0100

    Add files via upload

---
+++ Calculator_Qt/inc/Bin.h
@@ -0,0 +1,10 @@
+#ifndef BIN_H
+#define BIN_H
+#include <string>
+
+class Bin {
+public:
+    std::string FloatToBin(float value);
+    std::string ExpBinToDec(std::string exp);
+};
+#endif // BIN_H
---
+++ Calculator_Qt/inc/Oct.h
@@ -0,0 +1,10 @@
+#ifndef OCT_H
+#define OCT_H
+#include <string>
+
+class Oct{
+public:
+    std::string FloatToOct(float value);
+    std::string ExpOctToDec(std::string exp);
+};
+#endif // OCT_H
