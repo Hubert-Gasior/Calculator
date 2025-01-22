17e3c341d69ed8a92c55eaa0a76b01d540018f47
Hubert-Gasior <164912381+Hubert-Gasior@users.noreply.github.com>
2025-01-22 17:01:47 +0100

    Add files via upload

---
+++ Calculator_MFC/inc/resource.h
@@ -0,0 +1,34 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MFC.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFC_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1_Compute             1000
+#define IDC_EDIT1_exp                   1001
+#define IDC_EDIT3_float                 1003
+#define IDC_EDIT4_bin                   1004
+#define IDC_EDIT5_oct                   1005
+#define IDC_EDIT6_hex                   1006
+#define IDC_STATIC_float                1007
+#define IDC_STATIC_bin                  1008
+#define IDC_STATIC_oct                  1009
+#define IDC_STATIC_hex                  1010
+#define IDC_CHECK1_float                1011
+#define IDC_CHECK2_bin                  1012
+#define IDC_CHECK3_oct                  1013
+#define IDC_CHECK4_hex                  1014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1015
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
